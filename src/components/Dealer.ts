713acfcc2fcfab1e2ac7c59d7d202ecfbfe6d123
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-05-07 11:09:34 +0100

    Streamlined component

--- src/components/Dealer.ts
+++ src/components/Dealer.ts
@@ -1,67 +1,38 @@
 import axios from 'axios';
-
-export type PlayingCard = {
-  code: string 
-  image: string 
-  images: {
-    svg: string 
-    png: string
-  } 
-  value: string 
-  suit: string
-};
-
-// NOTE : pokerDeckID & blackjackDeckID are not secrets, 
-//        this is a public API anyone can use with no auth requirement & 
-//        these are just deck IDs it autogenerates that we are holding here to speed up access to decks
-
-const pokerDeckID = "hfj5xij3cuxy";
-const blackjackDeckID = "zogkuxg8362d"; 
+import { PlayingCard } from './Cards';
 
 export class Dealer {
-  deckID: string 
+  deckID: string
   nDecks: number
   
-  constructor(game:string){
-    if(game.toLowerCase() === "poker"){
-      this.deckID = pokerDeckID;
-      this.nDecks = 1
-    } 
-    
-    else if (game.toLowerCase() === "blackjack"){
-      this.deckID = blackjackDeckID;
-      this.nDecks =  6
-    }
-
-    else{
+  constructor(game:string, deckID:string, nDecks:number){
+    if ( !["blackjack", "poker",].includes(game.toLowerCase()) ){
       throw new Error("Please select a game : 'Blackjack' or 'Poker")
     }
-
+    this.deckID = deckID;
+    this.nDecks = nDecks
   }
 
-  async getDeck(): Promise<string> {
+  async getDeck() {
     // Get Deck 
-    if(this.deckID == null) {
+    let deckCheck = await axios.get(`https://deckofcardsapi.com/api/deck/${this.deckID}/shuffle/`).catch(
+      function(){return "Invalid deckID";}
+    );
+
+    if(deckCheck === "Invalid deckID") {
       await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${this.nDecks}`
         ).then(res => this.deckID = res.data.deck_id);
     }
 
     // Shuffle Deck
     await axios.get(`https://deckofcardsapi.com/api/deck/${this.deckID}/shuffle/`
-      ).then(res => this.deckID = res.data.deck_id);
-    
-    return this.deckID;
+      );
   }
 
   async dealCards(n:number): Promise<PlayingCard[]> {
-    if(this.deckID == null){
-      this.getDeck();
-    } 
-    
     // Draw n-cards from the deck
     let res = await axios.get(`https://deckofcardsapi.com/api/deck/${this.deckID}/draw/?count=${n}`);
     return res.data.cards;
-    
   };
 
   mergeHands (hands:PlayingCard[][]): PlayingCard[] {
@@ -71,8 +42,3 @@ export class Dealer {
 };
 
 }
-
-// let dealer = new Dealer("blackjack");
-
-// dealer.getDeck();
-// dealer.mergeHands([await dealer.dealCards(4), await dealer.dealCards(4)]);
